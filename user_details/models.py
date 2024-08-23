86253709efa30a522a1164b6767c1c5a24825537
giri-www <girirahul944@gmail.com>
2024-08-23 23:21:08 +0530

    MODELS COMMENT ADDED

--- user_details/models.py
+++ user_details/models.py
@@ -1,3 +1,7 @@
+"""
+## MODELS WITH SERIALIZERS ##
+INHERIT FROM '''AuditTimestampModel''' FOR DATETIME
+"""
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
