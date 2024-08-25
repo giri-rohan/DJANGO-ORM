3669707ec0ebcdc729760e8c624467ccb49f7f79
giri-www <girirahul944@gmail.com>
2024-08-25 23:40:11 +0530

    user details urls added

--- Djangoorm/urls.py
+++ Djangoorm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from drf_yasg.generators import OpenAPISchemaGenerator
@@ -64,4 +64,5 @@ urlpatterns = [
     path('swagger/', SchemaView.with_ui('swagger',
         cache_timeout=0), name='schema-swagger-ui'),       
     path('swagger/v2/', SchemaViewV2.with_ui('swagger', cache_timeout=0), name='swagger-v2'),
+    path('orm/user/', include('user_details.urls')),
 ]
