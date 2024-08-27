0b500d08dcc62ce9316babeab18f54811c2d589e
giri-www <girirahul944@gmail.com>
2024-08-28 01:13:50 +0530

    fix

--- Djangoorm/settings.py
+++ Djangoorm/settings.py
@@ -228,17 +228,3 @@ LOGGING = {
 }
 
 
-# EMAIL_HOST_PASSWORD='3VadFygDOWhEsZGH'
-# EMAIL_HOST_USER='7b0998001@smtp-brevo.com'
-# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST='smtp-relay.brevo.com'
-# # EMAIL_USE_TLS=True
-# EMAIL_PORT=587
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = '7b0998001@smtp-brevo.com'
-EMAIL_HOST_PASSWORD = '3VadFygDOWhEsZGH'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'rahul.giri1161@gmail.com'
