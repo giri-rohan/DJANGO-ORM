05fdbb57b931afd87784a2f9f27c146d09b2428d
giri-www <girirahul944@gmail.com>
2024-08-27 20:41:44 +0530

    generate otp added in url

--- user_details/urls.py
+++ user_details/urls.py
@@ -1,7 +1,10 @@
 ''' user_details Views URL'''
 from django.urls import path
-from .views import  CreateUser
+from .views import  CreateUser,GenerateOtp
+# VerifyOtpView
 
 urlpatterns = [
+    # path('verify-otp/', VerifyOtpView.as_view(), name='verify_otp'),
     path('create/', CreateUser.as_view(), name='signup'),
+    path('generateotp/',GenerateOtp.as_view(), name='generate_otp'),
 ]
