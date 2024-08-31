10e30a24532bfb8c9ed5dce71bf1d12a97387af9
giri-www <girirahul944@gmail.com>
2024-08-31 22:02:21 +0530

    urls  in user details changes

--- user_details/urls.py
+++ user_details/urls.py
@@ -1,10 +1,12 @@
 ''' user_details Views URL'''
 from django.urls import path
-from .views import  CreateUser,GenerateOtp,VerifyOtp
+from .views import  CreateUser,GenerateOtp,VerifyOtp,LoginUser,Health
 # VerifyOtpView
 
 urlpatterns = [
     path('verifyotp/', VerifyOtp.as_view(), name='verify_otp'),
     path('create/', CreateUser.as_view(), name='signup'),
+    path('login/',LoginUser.as_view(), name='login'),
     path('generateotp/',GenerateOtp.as_view(), name='generate_otp'),
+    path('test/',Health.as_view(), name='test')
 ]
